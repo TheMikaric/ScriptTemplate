8acdbff59a17b5433cbda45ba842328f52523aab
TheMikaric <md220088d@student.etf.bg.ac.rs>
2025-01-14 14:36:39 +0100

    Updated logging

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import scriptlibraries.myui as ui
 import scriptlibraries.myio as io 
 import scriptlibraries.mymainlogic as mainlogic
+import logging 
+
+logging.basicConfig(
+        level=logging.ERROR,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename='logs/main.log')
+
+logger = logging.getLogger(__name__)
+logger.level = logging.DEBUG
 
 print('Hello, world from main!')
+logger.debug('Hello, world from main!')
 ui.test()
 io.test()
 mainlogic.test()
