d6c0993e6e5755dd7f0d22f227f94aeebe34fa9b
TheMikaric <md220088d@student.etf.bg.ac.rs>
2025-02-18 15:43:04 +0100

    Testing!

---
+++ scriptlibraries/tests/test_mylogiclib.py
@@ -0,0 +1,25 @@
+import unittest as ut
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+import mylogiclib as lib
+
+class TestMylogiclibpy(ut.TestCase):
+
+    def test_resolve_file_name(self):
+        self.assertEqual(lib.resolve_file_name('A','B'),"B\\A")
+        self.assertEqual(lib.resolve_file_name('the_underscored','underscored_too'),'underscored_too\\the_underscored')
+        self.assertEqual(lib.resolve_file_name('file',''),'file')
+        with self.assertRaises(NameError):
+            lib.resolve_file_name('','subfolder')
+
+    def test_read_pdf(self):
+        self.assertEqual(lib.read_pdf('sample.pdf','scriptlibraries\\tests'),'sample! ')
+
+if __name__=='__main__':
+    ut.main()
